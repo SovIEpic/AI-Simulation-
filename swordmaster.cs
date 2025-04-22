c48d99902f9e7f2e978cbf685e1adc36e2d379a6
Ethan Gilman-Hawkes <42294109+Ethan34@users.noreply.github.com>
2025-04-22 20:00:10 +0100

    basic swordmaster skeleton
    - created stats
    - placeholder variable for distance to boss
    - placeholder attack class based on power and distance to boss
    - placeholder damage class based on boss power and distance to sowrdmaster
    - placeholder death class where death behaviour will go

---
+++ swordmaster.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Swordmaster {
+    int hp = 500;
+    int power = 200;
+    int armour = 100; 
+    int speed = 100;
+
+    // placeholder variable for distance to boss
+    // float distanceToBoss = 10f;
+
+    // placeholder class for attacking boss, where damage is based on the formula power / distance
+    // public void Attack(Boss boss) {
+        //    int damage = Math.Round(power / distanceToBoss);
+        //    Boss.TakeDamage(damage);
+        // }
+
+    // placeholder class for taking damage from boss, using same formula
+    // armour is subtracted from first, then health
+    // if the character's health drops to 0, it dies
+    // public void TakeDamage(int damage) {
+        // if (distanceToBoss >= 1 && distanceToBoss <= 5) {
+        //    int armourDamage = Math.Min(damage, armour);  
+        //    armour = armour - armourDamage;
+        //    int hpDamage = damage - armourDamage;
+        //    if (hpDamage > 0) {
+        //        hp = hp - hpDamage;
+        //        if (Health <= 0) {
+        //            Die();
+        //        }
+        //    }
+        // }
+    // }
+    
+    // placeholder class for dying, with no behaviour currently implemented
+    // public void Die() {
+    //     death behaviour will go here, i.e. disappearing from the game
+    // } 
+    }
+
